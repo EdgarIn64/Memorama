6e33ed7112877e8a9800a562537bdaa3405dc04f
EdgarIn64 <edgar_coriaSixtyFour@hotmail.com>
2023-08-30 12:25:58 -0600

    First commit

---
+++ Memorama/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Memorama
+{
+    partial class Memorama
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.img2 = new System.Windows.Forms.PictureBox();
+            this.img3 = new System.Windows.Forms.PictureBox();
+            this.img4 = new System.Windows.Forms.PictureBox();
+            this.img5 = new System.Windows.Forms.PictureBox();
+            this.img7 = new System.Windows.Forms.PictureBox();
+            this.img8 = new System.Windows.Forms.PictureBox();
+            this.img9 = new System.Windows.Forms.PictureBox();
+            this.img10 = new System.Windows.Forms.PictureBox();
+            this.img11 = new System.Windows.Forms.PictureBox();
+            this.img12 = new System.Windows.Forms.PictureBox();
+            this.img6 = new System.Windows.Forms.PictureBox();
+            this.img18 = new System.Windows.Forms.PictureBox();
+            this.img17 = new System.Windows.Forms.PictureBox();
+            this.img16 = new System.Windows.Forms.PictureBox();
+            this.img15 = new System.Windows.Forms.PictureBox();
+            this.img14 = new System.Windows.Forms.PictureBox();
+            this.img13 = new System.Windows.Forms.PictureBox();
+            this.titulo = new System.Windows.Forms.TextBox();
+            this.img1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img2
+            // 
+            this.img2.Image = global::Memorama.Resource2.oculto;
+            this.img2.Location = new System.Drawing.Point(148, 53);
+            this.img2.Name = "img2";
+            this.img2.Size = new System.Drawing.Size(130, 125);
+            this.img2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2.TabIndex = 2;
+            this.img2.TabStop = false;
+            this.img2.Visible = false;
+            this.img2.Click += new System.EventHandler(this.img2_Click);
+            // 
+            // img3
+            // 
+            this.img3.Image = global::Memorama.Resource2.oculto;
+            this.img3.Location = new System.Drawing.Point(284, 53);
+            this.img3.Name = "img3";
+            this.img3.Size = new System.Drawing.Size(130, 125);
+            this.img3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img3.TabIndex = 3;
+            this.img3.TabStop = false;
+            this.img3.Visible = false;
+            this.img3.Click += new System.EventHandler(this.img3_Click);
+            // 
+            // img4
+            // 
+            this.img4.Image = global::Memorama.Resource2.oculto;
+            this.img4.Location = new System.Drawing.Point(420, 53);
+            this.img4.Name = "img4";
+            this.img4.Size = new System.Drawing.Size(130, 125);
+            this.img4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img4.TabIndex = 4;
+            this.img4.TabStop = false;
+            this.img4.Visible = false;
+            this.img4.Click += new System.EventHandler(this.img4_Click);
+            // 
+            // img5
+            // 
+            this.img5.Image = global::Memorama.Resource2.oculto;
+            this.img5.Location = new System.Drawing.Point(556, 53);
+            this.img5.Name = "img5";
+            this.img5.Size = new System.Drawing.Size(130, 125);
+            this.img5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img5.TabIndex = 5;
+            this.img5.TabStop = false;
+            this.img5.Visible = false;
+            this.img5.Click += new System.EventHandler(this.img5_Click);
+            // 
+            // img7
+            // 
+            this.img7.Image = global::Memorama.Resource2.oculto;
+            this.img7.Location = new System.Drawing.Point(12, 184);
+            this.img7.Name = "img7";
+            this.img7.Size = new System.Drawing.Size(130, 125);
+            this.img7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img7.TabIndex = 6;
+            this.img7.TabStop = false;
+            this.img7.Visible = false;
+            // 
+            // img8
+            // 
+            this.img8.Image = global::Memorama.Resource2.oculto;
+            this.img8.Location = new System.Drawing.Point(148, 184);
+            this.img8.Name = "img8";
+            this.img8.Size = new System.Drawing.Size(130, 125);
+            this.img8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img8.TabIndex = 7;
+            this.img8.TabStop = false;
+            this.img8.Visible = false;
+            // 
+            // img9
+            // 
+            this.img9.Image = global::Memorama.Resource2.oculto;
+            this.img9.Location = new System.Drawing.Point(284, 184);
+            this.img9.Name = "img9";
+            this.img9.Size = new System.Drawing.Size(130, 125);
+            this.img9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img9.TabIndex = 8;
+            this.img9.TabStop = false;
+            this.img9.Visible = false;
+            // 
+            // img10
+            // 
+            this.img10.Image = global::Memorama.Resource2.oculto;
+            this.img10.Location = new System.Drawing.Point(420, 184);
+            this.img10.Name = "img10";
+            this.img10.Size = new System.Drawing.Size(130, 125);
+            this.img10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img10.TabIndex = 9;
+            this.img10.TabStop = false;
+            this.img10.Visible = false;
+            // 
+            // img11
+            // 
+            this.img11.Image = global::Memorama.Resource2.oculto;
+            this.img11.Location = new System.Drawing.Point(556, 184);
+            this.img11.Name = "img11";
+            this.img11.Size = new System.Drawing.Size(130, 125);
+            this.img11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img11.TabIndex = 10;
+            this.img11.TabStop = false;
+            this.img11.Visible = false;
+            // 
+            // img12
+            // 
+            this.img12.Image = global::Memorama.Resource2.oculto;
+            this.img12.Location = new System.Drawing.Point(692, 184);
+            this.img12.Name = "img12";
+            this.img12.Size = new System.Drawing.Size(130, 125);
+            this.img12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img12.TabIndex = 12;
+            this.img12.TabStop = false;
+            this.img12.Visible = false;
+            // 
+            // img6
+            // 
+            this.img6.Image = global::Memorama.Resource2.oculto;
+            this.img6.Location = new System.Drawing.Point(692, 53);
+            this.img6.Name = "img6";
+            this.img6.Size = new System.Drawing.Size(130, 125);
+            this.img6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img6.TabIndex = 11;
+            this.img6.TabStop = false;
+            this.img6.Visible = false;
+            this.img6.Click += new System.EventHandler(this.img6_Click);
+            // 
+            // img18
+            // 
+            this.img18.Image = global::Memorama.Resource2.oculto;
+            this.img18.Location = new System.Drawing.Point(692, 315);
+            this.img18.Name = "img18";
+            this.img18.Size = new System.Drawing.Size(130, 125);
+            this.img18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img18.TabIndex = 18;
+            this.img18.TabStop = false;
+            this.img18.Visible = false;
+            // 
+            // img17
+            // 
+            this.img17.Image = global::Memorama.Resource2.oculto;
+            this.img17.Location = new System.Drawing.Point(556, 315);
+            this.img17.Name = "img17";
+            this.img17.Size = new System.Drawing.Size(130, 125);
+            this.img17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img17.TabIndex = 17;
+            this.img17.TabStop = false;
+            this.img17.Visible = false;
+            // 
+            // img16
+            // 
+            this.img16.Image = global::Memorama.Resource2.oculto;
+            this.img16.Location = new System.Drawing.Point(420, 315);
+            this.img16.Name = "img16";
+            this.img16.Size = new System.Drawing.Size(130, 125);
+            this.img16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img16.TabIndex = 16;
+            this.img16.TabStop = false;
+            this.img16.Visible = false;
+            // 
+            // img15
+            // 
+            this.img15.Image = global::Memorama.Resource2.oculto;
+            this.img15.Location = new System.Drawing.Point(284, 315);
+            this.img15.Name = "img15";
+            this.img15.Size = new System.Drawing.Size(130, 125);
+            this.img15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img15.TabIndex = 15;
+            this.img15.TabStop = false;
+            this.img15.Visible = false;
+            // 
+            // img14
+            // 
+            this.img14.Image = global::Memorama.Resource2.oculto;
+            this.img14.Location = new System.Drawing.Point(148, 315);
+            this.img14.Name = "img14";
+            this.img14.Size = new System.Drawing.Size(130, 125);
+            this.img14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img14.TabIndex = 14;
+            this.img14.TabStop = false;
+            this.img14.Visible = false;
+            // 
+            // img13
+            // 
+            this.img13.Image = global::Memorama.Resource2.oculto;
+            this.img13.Location = new System.Drawing.Point(12, 315);
+            this.img13.Name = "img13";
+            this.img13.Size = new System.Drawing.Size(130, 125);
+            this.img13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img13.TabIndex = 13;
+            this.img13.TabStop = false;
+            this.img13.Visible = false;
+            // 
+            // titulo
+            // 
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(263, 3);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(310, 44);
+            this.titulo.TabIndex = 19;
+            this.titulo.Text = "FIGURAS";
+            this.titulo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // img1
+            // 
+            this.img1.Image = global::Memorama.Resource2.oculto;
+            this.img1.Location = new System.Drawing.Point(12, 53);
+            this.img1.Name = "img1";
+            this.img1.Size = new System.Drawing.Size(130, 125);
+            this.img1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1.TabIndex = 20;
+            this.img1.TabStop = false;
+            this.img1.Visible = false;
+            this.img1.Click += new System.EventHandler(this.img1_Click);
+            // 
+            // Memorama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 455);
+            this.Controls.Add(this.img1);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.img18);
+            this.Controls.Add(this.img17);
+            this.Controls.Add(this.img16);
+            this.Controls.Add(this.img15);
+            this.Controls.Add(this.img14);
+            this.Controls.Add(this.img13);
+            this.Controls.Add(this.img12);
+            this.Controls.Add(this.img6);
+            this.Controls.Add(this.img11);
+            this.Controls.Add(this.img10);
+            this.Controls.Add(this.img9);
+            this.Controls.Add(this.img8);
+            this.Controls.Add(this.img7);
+            this.Controls.Add(this.img5);
+            this.Controls.Add(this.img4);
+            this.Controls.Add(this.img3);
+            this.Controls.Add(this.img2);
+            this.Name = "Memorama";
+            this.Text = "Memorama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox img2;
+        private System.Windows.Forms.PictureBox img3;
+        private System.Windows.Forms.PictureBox img4;
+        private System.Windows.Forms.PictureBox img5;
+        private System.Windows.Forms.PictureBox img7;
+        private System.Windows.Forms.PictureBox img8;
+        private System.Windows.Forms.PictureBox img9;
+        private System.Windows.Forms.PictureBox img10;
+        private System.Windows.Forms.PictureBox img11;
+        private System.Windows.Forms.PictureBox img12;
+        private System.Windows.Forms.PictureBox img6;
+        private System.Windows.Forms.PictureBox img18;
+        private System.Windows.Forms.PictureBox img17;
+        private System.Windows.Forms.PictureBox img16;
+        private System.Windows.Forms.PictureBox img15;
+        private System.Windows.Forms.PictureBox img14;
+        private System.Windows.Forms.PictureBox img13;
+        private System.Windows.Forms.TextBox titulo;
+        private System.Windows.Forms.PictureBox img1;
+    }
+}
+
---
+++ Memorama/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memorama
+{
+    public partial class Memorama : Form
+    {
+        private string[] cartas;
+        private string[] colocadas;
+
+        public Memorama()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cartas = new string[] { "garza", "gato", "tortuga", "gusano", "perro", "rana", "rino", "tigre", "topo"};
+            string[] cartas_aux = cartas;
+            this.colocadas = new string[cartas.Length];
+
+            Random rnd = new Random();
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                int num = rnd.Next(1,18);
+                this.colocadas = new string[] { cartas_aux[num] };
+                cartas_aux = Array.FindAll(cartas_aux, (cartas_aux != "fas")).ToArray();
+
+                //cartas_aux = cartas_aux.Where(val => val != num).ToArray();
+
+            }
+
+        }
+
+        private void img1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void img2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void img3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void img4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void img5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void img6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
